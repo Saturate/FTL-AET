6982c043010de7244d5ddb92deb19de12c051878
Allan K. Jensen <allankimmerjensen@gmail.com>
2012-10-10 15:31:21 +0300

    Restored make.bat

--- make.bat
+++ make.bat
@@ -1,1 +1,5 @@
-This .bat does nothing. Why have it here?
+REM Requires 7-Zip to work.
+REM It is a quick and easy way to build a .ftl file.
+@Echo off
+DEL FTL-Additional-Events-and-Texts.ftl
+"C:\Program Files\7-Zip\7z.exe" a -tzip FTL-Additional-Events-and-Texts.ftl data
